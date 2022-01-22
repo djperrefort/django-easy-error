9f1b0063fcd47b91b44ee32c7b7e1527d0d58ac5
Daniel <djperrefort@gmail.com>
2022-01-21 20:57:50 -0500

    Simplifies code in the get method of generated views

--- django_simple_error/factory.py
+++ django_simple_error/factory.py
@@ -1,4 +1,4 @@
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
@@ -6,20 +6,28 @@ from django.views import View
 
 
 def view_factory(error_code: int) -> callable:
-    """Factory function for creating class based django views that render custom error tempaltes
+    """Factory function for creating class based django views that render custom error templates
 
     Args:
         error_code: The error code handled by the view
     """
 
-    def get(self, request: HttpRequest, *args, **kwargs) -> None:
+    try:
+        template = 'django_simple_error/http_{}.html'.format(error_code)
+        get_template(template)
 
-        try:
-            template = 'django_simple_error/http_{}.html'.format(error_code)
-            get_template(self.template_name)
+    except TemplateDoesNotExist:
+        template = 'django_simple_error/default.html'
 
-        except TemplateDoesNotExist:
-            template = 'django_simple_error/default.html'
+    def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """Respond to an incoming HTTP request
+
+        Args:
+            request: The response to respond to
+
+        Returns:
+            A rendered HML response
+        """
 
         return render(request, template, status=error_code, context={'error_code': error_code})
 
